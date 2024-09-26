7e653cc01908e957accb5a513cd2cf4ae839a0a4
Sabin Pandey <49552299+sabeen2@users.noreply.github.com>
2024-09-26 15:12:40 +0545

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,15 +1,15 @@
-import { isDevelopment } from "./env";
+// import { isDevelopment } from "./env";
 
-export const server = !isDevelopment
-  ? "https://multi-vendor-ecommerce-silk.vercel.app/api/v2"
-  : "http://localhost:8000/api/v2";
+// export const server = !isDevelopment
+//   ? "https://multi-vendor-ecommerce-silk.vercel.app/api/v2"
+//   : "http://localhost:8000/api/v2";
 
-export const backend_url = !isDevelopment
-  ? "https://multi-vendor-ecommerce-silk.vercel.app/"
-  : "http://localhost:8000/";
+// export const backend_url = !isDevelopment
+//   ? "https://multi-vendor-ecommerce-silk.vercel.app/"
+//   : "http://localhost:8000/";
 
-// export const server = "https://multi-vendor-ecommerce-silk.vercel.app/api/v2";
-// export const backend_url = "https://multi-vendor-ecommerce-silk.vercel.app/";
+export const server = "https://multi-vendor-ecommerce-silk.vercel.app/api/v2";
+export const backend_url = "https://multi-vendor-ecommerce-silk.vercel.app/";
 
 // export const server = "http://localhost:8000/api/v2";
 // export const backend_url = "http://localhost:8000/";
